92696bd31400b7751aa3a9cf3b4fa413dc587a6e
SamSchmakel28 <samuel.schmakel@gmail.com>
2025-03-17 13:32:55 -0500

    Implemented reading 8 bytes at a time

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const inputFilePath = "messages.txt"
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("Error opening text file: %v", err)
+	}
+	defer file.Close()
+
+	for {
+		buffer := make([]byte, 8)
+		n, err := file.Read(buffer)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Println("Error reading file:", err.Error())
+			break
+		}
+		str := string(buffer[:n])
+		fmt.Printf("read: %s\n", str)
+	}
+
 }
