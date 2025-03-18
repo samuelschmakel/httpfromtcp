a7274efcb1b8621c6bb574a655ddcde43b98b90c
SamSchmakel28 <samuel.schmakel@gmail.com>
2025-03-17 19:25:47 -0500

    Added getLinesChannel function

--- main.go
+++ main.go
@@ -17,28 +17,41 @@ func main() {
 		log.Fatalf("Error opening text file: %v", err)
 	}
 	defer file.Close()
+	
+	ch := getLinesChannel(file)
+	for value := range ch {
+		fmt.Printf("read: %s\n", value)
+	}
 
-	currentLineContents := ""
+}
 
-	for {
-		buffer := make([]byte, 8)
-		n, err := file.Read(buffer)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				if currentLineContents != "" {
-					fmt.Printf("read: %s\n", currentLineContents)
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	go func() {
+		defer close(ch)
+		currentLineContents := ""
+
+		for {
+			buffer := make([]byte, 8)
+			n, err := f.Read(buffer)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					if currentLineContents != "" {
+						ch <- currentLineContents
+					}
+					break
 				}
+				fmt.Println("Error reading file:", err.Error())
 				break
 			}
-			fmt.Println("Error reading file:", err.Error())
-			break
-		}
 		str := string(buffer[:n])
 		parts := strings.Split(str, "\n")
 		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s%s\n", currentLineContents, parts[i])
+			ch <- currentLineContents + parts[i]
 			currentLineContents = ""
 		}
 		currentLineContents += parts[len(parts)-1]
-	}
+		}
+	}()
+	return ch
 }
