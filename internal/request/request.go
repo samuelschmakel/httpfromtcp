3fb6be5412ca1d177ff9e9c2cbd57cd81f6353f3
SamSchmakel28 <samuel.schmakel@gmail.com>
2025-03-24 18:21:29 -0500

    Refactored code to read differently sized chunks of bytes

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,7 @@
 package request
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"io"
@@ -9,6 +10,7 @@ import (
 
 type Request struct {
 	RequestLine RequestLine
+	State int // 0 for "initialized", 1 for "done"
 }
 
 type RequestLine struct {
@@ -17,56 +19,124 @@ type RequestLine struct {
 	Method string
 }
 
+const crlf = "\r\n"
+const bufferSize = 8
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	bs, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, fmt.Errorf("error reading request: %s", err.Error())
-	}
-	str := string(bs)
-	newLine := "\r\n"
-
-	index := strings.Index(str, newLine)
-	if index == -1 {
-		return nil, errors.New("invalid request format")
-	}
-	requestLineStr := str[:index]
-
-	requestLineData := strings.Split(requestLineStr, " ")
-	if len(requestLineData) != 3 {
-		return nil, fmt.Errorf("invalid request format. Actual length of request data is: %d", len(requestLineData))
+	r := &Request{
+		State: 0,
 	}
 
-	requestLine := RequestLine{
-		HttpVersion: requestLineData[2],
-		RequestTarget: requestLineData[1],
-		Method: requestLineData[0],
-	}
+	buf := make([]byte, bufferSize)
+	readToIndex := 0
 
-	if !isAllUpper(requestLine.Method) {
-		return nil, errors.New("invalid method format")
-	}
+	for r.State != 1 {
+		if readToIndex >= len(buf) {
+			newBuf := make([]byte, len(buf)*2)
+			copy(newBuf, buf)
+			buf = newBuf
+		}
 
-	if requestLine.HttpVersion != "HTTP/1.1" {
-		return nil, errors.New("unexpected HTTP version")
-	}
+		n, err := reader.Read(buf[readToIndex:])
+		// Handle real errors
+		if err != nil && err != io.EOF {
+			return nil, err
+		} 
+		readToIndex += n
 
-	requestLine.HttpVersion = "1.1"
+		parsedBytes, parseErr := r.parse(buf[:readToIndex])
+		if parseErr != nil {
+			return nil, parseErr
+		}
 
-	if !strings.Contains(requestLine.RequestTarget, "/") {
-		return nil, errors.New("invalid request target")
-	}
+		if parsedBytes > 0 {
+			copy(buf, buf[parsedBytes:readToIndex])
+			readToIndex -= parsedBytes
+		}
 
-	req := Request{
-		RequestLine: requestLine,
-	}
-	return &req, nil
-}
+		if err == io.EOF {
+			if readToIndex == 0 || r.State == 1 {
+				break
+			}
 
-func isAllUpper(s string) bool {
-	for _, r := range s {
-		if r < 'A' || r > 'Z' {
-			return false
+			return nil, errors.New("error: incomplete request")
 		}
 	}
-	return true
+
+	return r, nil
+}
+
+// TODO: Rewrite this function to return the number of bytes it consumed (and for functionality)
+func parseRequestLine(data []byte) (requestline *RequestLine, numBytes int, err error) {
+	idx := bytes.Index(data, []byte(crlf))
+	if idx == -1 {
+		return nil, 0, nil
+	}
+	requestLineText := string(data[:idx])
+	requestLine, err := requestLineFromString(requestLineText)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return requestLine, idx + len(crlf), nil
+}
+
+func requestLineFromString(str string) (*RequestLine, error) {
+	parts := strings.Split(str, " ")
+	if len(parts) != 3 {
+		return nil, fmt.Errorf("poorly formatted request-line: %s", str)
+	}
+
+	method := parts[0]
+
+	// Checks for only capital letters
+	for _, c := range method {
+		if c < 'A' || c > 'Z' {
+			return nil, fmt.Errorf("invalid method: %s", method)
+		}
+	}
+
+	requestTarget := parts[1]
+
+	versionParts := strings.Split(parts[2], "/")
+	if len(versionParts) != 2 {
+		return nil, fmt.Errorf("malformed start-line %s", str)
+	}
+
+	httpPart := versionParts[0]
+	if httpPart != "HTTP" {
+		return nil, fmt.Errorf("unrecognized HTTP-version: %s", httpPart)
+	}
+	version := versionParts[1]
+	if version != "1.1" {
+		return nil, fmt.Errorf("unrecognized HTTP-version: %s", version)
+	}
+
+	return &RequestLine{
+		Method: method,
+		RequestTarget: requestTarget,
+		HttpVersion: versionParts[1],
+	}, nil
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+	// "Initialized" state
+	if r.State == 0 {
+		requestLine, numBytes, err := parseRequestLine(data)
+		if err != nil {
+			return 0, err
+		}
+
+		if numBytes == 0 {
+			return 0, nil
+		}
+
+		r.RequestLine = *requestLine
+		r.State = 1 // "Done" state
+		return numBytes, nil
+	} else if r.State == 1 {
+		return 0, errors.New("error: trying to read data in a done state")
+	} else {
+		return 0, errors.New("error: unknown state")
+	}
 }
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -10,7 +10,11 @@ import (
 
 func TestRequestLineParse (t *testing.T) {
 	// Test: Good GET Request Line
-	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUserAgent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	reader := &chunkReader{
+		data: "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 3,
+	}
+	r, err := RequestFromReader(reader)
 	require.NoError(t, err)
 	require.NotNil(t, r)
 	assert.Equal(t, "GET", r.RequestLine.Method)
@@ -18,7 +22,11 @@ func TestRequestLineParse (t *testing.T) {
 	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
 
 	// Test: Good GET Request line with path
-	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	reader = &chunkReader{
+		data: "GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 1,
+	}
+	r, err = RequestFromReader(reader)
 	require.NoError(t, err)
 	require.NotNil(t, r)
 	assert.Equal(t, "GET", r.RequestLine.Method)
