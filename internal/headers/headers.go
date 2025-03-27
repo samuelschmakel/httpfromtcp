84aca3d7cb6bae152ed8130f1a1cfc07b98b2d6c
SamSchmakel28 <samuel.schmakel@gmail.com>
2025-03-27 18:04:56 -0500

    Added HTTP header parsing functionality

---
+++ internal/headers/headers.go
@@ -0,0 +1,57 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+type Headers map[string]string
+
+const crlf = "\r\n"
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	fmt.Println("New run")
+	idx := bytes.Index(data, []byte(crlf))
+
+	// Assume not enough data
+	if idx == -1 {
+		return 0, false, nil
+	}
+
+	// End of headers
+	if idx == 0 {
+		return len(crlf), true, nil
+	}
+
+	headersText := string(data[:idx])
+
+	// Removes trailing and leading whitespace
+	headersText = strings.TrimSpace(headersText)
+
+	// Split on first colon
+	colonIdx := strings.Index(headersText, ":")
+	if colonIdx == -1 {
+		return 0, false, fmt.Errorf("invalid header: missing colon")
+	}
+
+	fieldName, fieldValue := headersText[:colonIdx], headersText[colonIdx+len(":"):]
+
+	if unicode.IsSpace(rune(fieldName[len(fieldName)-1])) {
+		return 0, false, fmt.Errorf("invalid header: whitespace between field name and colon")
+	}
+
+	fieldName = strings.TrimSpace(fieldName)
+	fieldValue = strings.TrimSpace(fieldValue)
+
+	// Add it to the map
+	h[fieldName] = fieldValue
+
+	
+	return idx + len(crlf), false, nil
+}
+
+func NewHeaders() Headers {
+	return map[string]string{}
+}
---
+++ internal/headers/headers_test.go
@@ -0,0 +1,58 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHeadersParse (t *testing.T) {
+	// Test: Valid Single header
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	// Test: Valid single header with extra whitespace
+	headers = NewHeaders()
+	data = []byte("     Host:        localhost:42069        \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 43, n)
+	assert.False(t, done)
+
+	// Test: Valid 2 headers with existing headers
+	headers = map[string]string{"host": "localhost:42069"}
+	data = []byte("User-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 25, n)
+	assert.False(t, done)
+
+	// Valid done
+	headers = NewHeaders()
+	data = []byte("\r\n other stuff")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Empty(t, headers)
+	assert.Equal(t, 2, n)
+	assert.True(t, done)
+
+	// Test: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("			Host : localhost:42069		\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+}
