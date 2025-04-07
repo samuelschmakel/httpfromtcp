fe6b6ae39c36a30b213b5b2ee0da5de75d6141c5
SamSchmakel28 <samuel.schmakel@gmail.com>
2025-04-07 10:27:36 -0500

    Implemented handler for request handling

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"httpfromtcp/internal/request"
+	"httpfromtcp/internal/response"
 	"httpfromtcp/internal/server"
+	"io"
 	"log"
 	"os"
 	"os/signal"
@@ -10,8 +13,9 @@ import (
 
 const port = 42069
 
+// TODO: Figure out why tha handler isn't being accepted in the serve.Serve call
 func main() {
-	server, err := server.Serve(port)
+	server, err := server.Serve(port, handler)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
@@ -22,4 +26,21 @@ func main() {
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	<-sigChan
 	log.Println("Server gracefully stopped")
+}
+
+func handler(w io.Writer, req *request.Request) *server.HandlerError {
+	if req.RequestLine.RequestTarget == "/yourproblem" {
+		return &server.HandlerError{
+			StatusCode: response.StatusCodeBadRequest,
+			Message: "Your problem is not my problem\n",
+		}
+	} else if req.RequestLine.RequestTarget == "/myproblem" {
+		return &server.HandlerError{
+			StatusCode: response.StatusCodeInternalServerError,
+			Message: "Woopsie, my bad\n",
+		}
+	} else {
+		w.Write([]byte("All good, frfr\n"))
+	}
+	return nil
 }
